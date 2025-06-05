e48d4368f5a22037bdd40fd90fcdfc12faad37df
karol-kkk <karol.m99@interia.pl>
2025-06-05 12:23:08 +0200

    update

--- webapp/forum/models.py
+++ webapp/forum/models.py
@@ -35,7 +35,7 @@ class Comment(models.Model):
 class PostImage(models.Model):
     post = models.ForeignKey(Posts, on_delete=models.CASCADE, related_name='images')
     image = models.ImageField(upload_to='post_images/', blank=True, null=True)
-    description = models.CharField(max_length=255, blank=True)
+    description = models.CharField(max_length=555, blank=True)
 
     def __str__(self):
         return f"Image for Post {self.post.id}"
